3895dc5fdf6406fe0908cf804f321d06f833f310
SrimannarayanP <srimannarayanpaidisetty@gmail.com>
2026-07-16 19:36:07 +0530

    Added more logic for the core game along with storage sync from Redis to Postgres

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -4,14 +4,14 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.core.exceptions import ObjectDoesNotExist
-from .models import GameSession, Quiz
+from .models import GameSession, PlayerResult, Quiz
 
 import redis.asyncio as redis
 
 import json, os, time, uuid
 
 
-redis_client = redis.from_url(os.environ.get('REDIS_URL'), decode_responses = True)
+redis_client = redis.from_url(os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0'), decode_responses = True)
 
 
 class GameConsumer(AsyncJsonWebsocketConsumer):
@@ -38,6 +38,8 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
 
         if action == 'host_start' and is_host:
             await self.handle_host_start()
+        elif action == 'host_next_question' and is_host:
+            await self.handle_next_question()
         elif action == 'player_join' and not is_host:
             await self.handle_player_join(content.get('data'))
         elif action == 'submit_answer' and not is_host:
@@ -49,6 +51,8 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         """Gets the pre-compiled JSON from Postgres, loads into Redis & starts the game"""
         quiz_data = await self.get_compiled_quiz_data()
 
+        first_question = quiz_data['questions'][0]
+
         state_key = f'game:{self.pin}:state' # Stores active state in a Redis hash
         
         await redis_client.hset(
@@ -56,9 +60,6 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
             mapping = {'status' : 'active', 'current_question_index' : 0, 'current_question_id' : first_question['id'], 'start_time' : time.time()}
         )
         await redis_client.set(f'game:{self.pin}:quiz', json.dumps(quiz_data)) # Stores the entire compiled quiz in Redis as a JSON str for faster reads
-
-        # Broadcast the 1st question to all connected clients
-        first_question = quiz_data['questions'][0]
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -84,6 +85,8 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         await redis_client.hset(f'game:{self.pin}:players', player_id, full_name) # Redis Hash: Map UUID to name
         await redis_client.zadd(f'game:{self.pin}:scores', {player_id : 0}) # Redis Sorted Set: Add to leaderboard with 0 points
         
+        await self.create_player_result(player_id, full_name, data.get('contact_info'), data.get('school_name'), data.get('grade_level'))
+
         await self.send_json({'event_type' : 'join_success', 'player_id' : player_id})
         await self.channel_layer.group_send(self.room_group_name, {'type' : 'broadcast_event', 'event_type' : 'player_joined', 'data' : {'name' : full_name}})
 
@@ -140,6 +143,46 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         # Send a msg to the entire room that the frontend can update the ans submitted counter.
         await self.channel_layer.group_send(self.room_group_name, {'type' : 'broadcast_event', 'event_type' : 'answer_registered'})
 
+    async def handle_next_question(self):
+        state_key = f'game:{self.pin}:state'
+        state = await redis_client.hgetall(state_key)
+
+        # Fetch current index
+        current_index = int(state.get('current_question_index', 0))
+        next_index = current_index + 1
+
+        # Load compiled quiz
+        quiz_data = json.loads(await redis_client.get(f'game:{self.pin}:quiz'))
+        total_questions = len(quiz_data['questions'])
+
+        # Check if game is over
+        if next_index >= total_questions:
+            await self.handle_end_game()
+
+            return
+
+        # Fetch next Q data
+        next_question = quiz_data['questions'][next_index]
+
+        # Change state to new Q
+        await redis_client.hset(state_key, mapping = {'current_question_index' : next_index, 'current_question_id' : next_question['id'], 'start_time' : time.time()})
+
+        # Broadcast to room
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type' : 'broadcast_event',
+                'event_type' : 'question_revealed',
+                'data' : {
+                    'question_id' : next_question['id'],
+                    'text' : next_question['text'],
+                    'time_limit' : next_question['time_limit'],
+                    'choices' : [{'id' : c['id'], 'text' : c['text']} for c in next_question['choices']]
+                }
+            }
+        )
+
+
     """DB readers"""
     @database_sync_to_async
     def get_game_session(self, pin):
@@ -155,5 +198,14 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
     def get_compiled_quiz_data(self):
 
         return self.session.quiz.compiled_data
-        
     
+    @database_sync_to_async
+    def create_player_result(self, player_id, full_name, contact_info, school_name, grade_level):
+        PlayerResult.objects.create(
+            session = self.session,
+            player_id = player_id,
+            full_name = full_name,
+            contact_info = contact_info,
+            school_name = school_name,
+            grade_level = grade_level
+        )
